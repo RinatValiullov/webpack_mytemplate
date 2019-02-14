9141a2c0f8db2dc6ff7e5ef6037706d8dd3e0935
RinatValiullov <4rontender@gmail.com>
2019-02-14 16:57:24 +0500

    added vue and bootstrap
    added some examples of import files and libraries

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './js/check';
+
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import Bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
+
+// import 'vue';
+// import Vue from 'vue';
+
+// window.Vue = require('vue');
